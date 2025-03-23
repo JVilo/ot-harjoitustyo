b652d890ac4deec37d35d332c432c7899a6f0796
JVilo <vilo.johanna@gmail.com>
2025-03-23 17:45:54 +0200

    added task.py fore invoke

---
+++ tasks.py
@@ -0,0 +1,16 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+
+@task
+def build(ctx):
+    ctx.run("python3 src/build.py", pty=True)
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
