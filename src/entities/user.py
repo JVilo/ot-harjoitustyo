4686a3d62a22279848f5651a00d9e83ee427d561
JVilo <vilo.johanna@gmail.com>
2025-03-24 21:50:13 +0200

    one test done and passed

--- src/entities/user.py
+++ src/entities/user.py
@@ -1,4 +1,7 @@
 class User:
     def __init__(self, username, password):
-        self.usernaem = username
+        self.username = username
         self.password = password
+    
+    def __str__(self):
+        return self.username
