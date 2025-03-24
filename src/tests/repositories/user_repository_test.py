8b07fed57a0afba83ff59e750709f53cb4abb19c
JVilo <vilo.johanna@gmail.com>
2025-03-24 21:21:32 +0200

    tests still under process not working

---
+++ src/tests/repositories/user_repository_test.py
@@ -0,0 +1,16 @@
+import unittest
+from repositories.user_repository import user_repository
+from entities.user import User
+
+class TestUserRepository(unittest.TestCase):
+    def setUp(self):
+        user_repository.delete_all()
+        self.user_eva = User('Eva', 'Eva321')
+        self.user_eino = User('Eino', 'Eino456')
+
+    def test_create(self):
+        user_repository.create(self.user_eva)
+        users = user_repository.find_all()
+
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0].username, self.user_eva.usernaem)
